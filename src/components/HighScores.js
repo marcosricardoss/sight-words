bf3c893b7038702f0f18f3dd680af4df302df227
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-24 15:53:06 -0300

    refactor: user FlatList on the High Scores component.

--- src/components/HighScores.js
+++ src/components/HighScores.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { FlatList, View, Text, StyleSheet } from 'react-native';
 
 const DEFAULT_TOTAL_NUMBER = 10;
 
@@ -9,11 +9,20 @@ export default HighScores = ({ data, totalNumber }) => {
   return (
     <View style={styles.container}>
       <Text style={styles.header}>High Scores</Text>
-      <TableHeader />
-      {
-        highScores.map((highScore, index) =>
-          <Row highScore={highScore} index={index} key={index} />)
-      }
+      <FlatList
+        data={highScores}
+        renderItem={
+          ({ item, index }) => 
+            <Row highScore={item} index={index} key={index} />
+        } 
+        keyExtractor={(item, index) => index.toString()}
+        ListEmptyComponent={() =>
+          <Text style={{ textAlign: 'center' }}>
+            There are no High Scores yet!
+          </Text>
+        }
+        ListHeaderComponent={() => highScores.length > 0 && <TableHeader />}
+      /> 
     </View>
   );
 };
@@ -52,7 +61,8 @@ const getTopScores = (highScores, totalNumber) =>
 const styles = StyleSheet.create({
   container: {
     width: 300,
-    marginBottom: 15
+    marginBottom: 40,
+    marginTop: 50
   },
   header: {
     textAlign: 'center',
