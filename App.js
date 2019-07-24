2978178b85b84a38bdf0a55c3f4d77d30445a2f5
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-24 13:25:18 -0300

    feat: customize the bottom tab navigation.

--- App.js
+++ App.js
@@ -4,12 +4,15 @@ import {
   createSwitchNavigator, 
   createBottomTabNavigator, 
   createAppContainer } from 'react-navigation';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import PracticeScreen from './src/screens/PracticeScreen';
 import ResultsScreen from './src/screens/ResultsScreen';
 import HighScoresScreen from './src/screens/HighScoresScreen';
 import SettingsScreen from './src/screens/SettingsScreen';
+
 
 class App extends Component {
   render() {
@@ -23,11 +26,39 @@ const HomeNavigator = createSwitchNavigator({
   Results: ResultsScreen
 });
 
-const AppNavigator = createBottomTabNavigator({
-  Home: HomeNavigator,
-  HighScores: HighScoresScreen,
-  Settings: SettingsScreen
-});
+const AppNavigator = createBottomTabNavigator(
+  {
+    Home: {
+      screen: HomeNavigator,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) =>
+          <FontAwesome5 name="home" size={25} color={tintColor} />
+      },
+    },
+    HighScores: {
+      screen: HighScoresScreen,    
+      navigationOptions: {
+        tabBarLabel: 'High Scores',
+        tabBarIcon: ({tintColor}) =>
+          <FontAwesome5 name="chart-bar" size={25} color={tintColor} />
+      },
+    },
+    Settings: {
+      screen: SettingsScreen,
+      navigationOptions: {
+        tabBarIcon: ({tintColor}) =>
+          <FontAwesome5 name="cogs" size={25} color={tintColor} />
+      },
+    }
+  },
+  {
+    tabBarOptions: {
+      activeTintColor: 'orange',
+      inactiveTintColor: 'gray',
+      showLabel: false
+    }
+  }
+);
 
 export default createAppContainer(AppNavigator);
 
