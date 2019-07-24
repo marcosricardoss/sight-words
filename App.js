3d4696304d3decdf73c16fe02fd3a8c47ec33c57
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-24 09:09:48 -0300

    refactor: extract our initial screen from the App.js file into a separate Welcome screen.

--- App.js
+++ App.js
@@ -1,31 +1,22 @@
 import React, { Component } from 'react';
-import { AppRegistry, Button, Text, View } from 'react-native';
+import { AppRegistry } from 'react-native';
 import { createSwitchNavigator, createAppContainer } from 'react-navigation';
 
-import styles from './src/styles';
+import WelcomeScreen from './src/screens/WelcomeScreen';
 import PracticeScreen from './src/screens/PracticeScreen';
 import ResultsScreen from './src/screens/ResultsScreen';
 
 class App extends Component {
   render() {
-    return(
-      <View style={styles.container}>
-        <View>
-          <Text style={styles.welcome}>Welcome to Blitz Reading!</Text>
-          <Button 
-            onPress={() => this.props.navigation.navigate('Practice')}
-            title="Practice" />
-        </View>
-      </View>
-    );
+    return <AppNavigator />
   }
 }
 
-const AppNavigator = createSwitchNavigator({
-  App: App,
+const HomeNavigator = createSwitchNavigator({
+  Welcome: WelcomeScreen,
   Practice: PracticeScreen,
   Results: ResultsScreen
 });
-export default createAppContainer(AppNavigator);
+export default createAppContainer(HomeNavigator);
 
 AppRegistry.registerComponent('BlitzReading', () => App);
