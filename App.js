c675224b49055084dc3e4f9e8a1d2679418e669f
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-24 09:40:11 -0300

    feat: add a main tab navigator.

--- App.js
+++ App.js
@@ -1,14 +1,19 @@
 import React, { Component } from 'react';
 import { AppRegistry } from 'react-native';
-import { createSwitchNavigator, createAppContainer } from 'react-navigation';
+import { 
+  createSwitchNavigator, 
+  createBottomTabNavigator, 
+  createAppContainer } from 'react-navigation';
 
 import WelcomeScreen from './src/screens/WelcomeScreen';
 import PracticeScreen from './src/screens/PracticeScreen';
 import ResultsScreen from './src/screens/ResultsScreen';
+import HighScoresScreen from './src/screens/HighScoresScreen';
+import SettingsScreen from './src/screens/SettingsScreen';
 
 class App extends Component {
   render() {
-    return <AppNavigator />
+    return <HomeNavigator />
   }
 }
 
@@ -17,6 +22,13 @@ const HomeNavigator = createSwitchNavigator({
   Practice: PracticeScreen,
   Results: ResultsScreen
 });
-export default createAppContainer(HomeNavigator);
+
+const AppNavigator = createBottomTabNavigator({
+  Home: HomeNavigator,
+  HighScores: HighScoresScreen,
+  Settings: SettingsScreen
+});
+
+export default createAppContainer(AppNavigator);
 
 AppRegistry.registerComponent('BlitzReading', () => App);
