fbf4b03b3ef75177b64f606d543644cd5333b23c
Marcos Ricardo <marcosricardoss@gmail.com>
2019-07-24 22:42:43 -0300

    feat: add LanguageSelectorScreen

---
+++ src/screens/LanguageSelectorScreen.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Text, View } from 'react-native';
+import styles from '../styles';
+
+class LanguageSelectorScreen extends React.Component {
+  static navigationOptions = {
+    title: 'Language'
+  };
+  
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Language selector Screen</Text>
+      </View>
+    );
+  }
+}
+
+export default LanguageSelectorScreen;
